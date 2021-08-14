a3595c9264d846580c270e6ae26a4f817f674292
SHAHAB MALIK <74496590+shahabmalik996@users.noreply.github.com>
2021-08-14 20:21:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const menue = document.querySelectorAll(".menue .member");
+console.log(menue);
+menue.forEach(item => {
+    item.addEventListener("click", () => {
+        var current = document.querySelectorAll(".active");
+        current[0].classList.remove("active");
+        item.classList.add("active");
+    });
+});
+
+const menueOpen = document.querySelector(".open");
+const menueClose = document.querySelector(".close");
+
+menueOpen.addEventListener("click", e => {
+    menueOpen.style.display = "none";
+    menueClose.style.display = "block";
+    const nav = document.querySelector("header nav");
+    nav.classList.add("show");
+});
+menueClose.addEventListener("click", e => {
+    menueClose.style.display = "none";
+    menueOpen.style.display = "block";
+    console.log(menueOpen);
+    const nav = document.querySelector("header nav");
+    nav.classList.remove("show");
+});
+
+function displayWindowSize() {
+    // Get width and height of the window excluding scrollbars
+    var w = document.documentElement.clientWidth;
+
+    // Display result inside a div element
+    // document.getElementById("result").innerHTML = "Width: " + w + ", " + "Height: " + h;
+    if (w > 990) {
+        const menu = document.querySelector(".menue-toggle");
+        menueClose.style.display = "none";
+        menueOpen.style.display = "none";
+    } else {
+        menueOpen.style.display = "block";
+    }
+}
+
+// Attaching the event listener function to window's resize event
+window.addEventListener("resize", displayWindowSize);
+
+// Calling the function for the first time
+displayWindowSize();
